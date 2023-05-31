c0e605ca37a400021a78f49b873897dddfe7c82e
Alex V <atv17998@gmail.com>
2023-05-30 23:32:31 -0400

    Everything sucks

---
+++ Project2/Header.h
@@ -0,0 +1,13 @@
+namespace N
+{
+    class login {
+    public:
+        char* user;
+        char* pass;
+        login(char* x, char* y) {
+            user = x;
+            pass = y;
+        }
+    };
+
+}
---
+++ Project2/Source.cpp
@@ -0,0 +1,22 @@
+//GOALS 
+//  Learn to work with classes and general object oriented stuff
+//  Just get better at problem solving through code
+
+#include <stdio.h>
+#include "class.hpp"
+
+
+int main(void) {
+    char user1[32];
+    char pass1[32];
+    printf("Register a username and password\n");
+    printf("Enter username: ");
+    scanf_s("%s", user1, 32);
+    printf("Enter password: ");
+    scanf_s("%s", pass1, 32);
+
+    login login1(user1, pass1);
+
+    printf("User = %s pass = %s", login1.user, login1.pass);
+    return(0);
+}
